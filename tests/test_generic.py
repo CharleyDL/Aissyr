24bc4f2d5ff652aac010a1f70eaf31c29c4fea1f
Charley ∆. L. <CharleyDL@users.noreply.github.com>
2024-03-27 19:51:30 +0100

    add test - class TestRequiredElements for folders and files

---
+++ tests/test_generic.py
@@ -0,0 +1,23 @@
+import os
+import pytest
+
+
+class TestVitalElements:
+
+    def test_folder_exists(self):
+        required_folders = ['.streamlit', 'asset', 'pages', 
+                            'streamlit_img_label', 'utils']
+        for folder in required_folders:
+            assert os.path.exists(folder)
+
+
+    def test_file_exists(self):
+        required_files = ['Home.py', 'requirements.txt', 
+                          'asset/logo_landing.png', 'asset/logo_aissyr_S.png', 
+                          'asset/logo_aissyr_M.png', 'asset/logo_aissyr_L.png', 
+                          'pages/main_page.py', 'pages/login.py', 
+                          'pages/register.py', 'pages/annotation.py',
+                          'pages/archive.py', 'pages/detect.py']
+
+        for file in required_files:
+            assert os.path.exists(file)
