cd51a1d3eaf689849ee77c883a8a7839754e579c
Charley ∆. L. <CharleyDL@users.noreply.github.com>
2024-04-06 12:36:28 +0200

    update api

--- utils/functions.py
+++ utils/functions.py
@@ -9,10 +9,10 @@ from pydantic import BaseModel
 DAGSHUB_USER_TOKEN='a4069f9e2a4a499a4a760c37d7890d1d775d651b'
 DAGSHUB_REPO_OWNER = "CharleyDL"
 DAGSHUB_REPO = "neo_aissyr"
-dagshub.init(DAGSHUB_REPO, DAGSHUB_REPO_OWNER)
+# dagshub.init(DAGSHUB_REPO, DAGSHUB_REPO_OWNER)
 
-remote_server_uri="https://dagshub.com/CharleyDL/neo_aissyr.mlflow"
-mlf.set_tracking_uri(remote_server_uri)
+# remote_server_uri="https://dagshub.com/CharleyDL/neo_aissyr.mlflow"
+# mlf.set_tracking_uri(remote_server_uri)
 
 # model_uri = 'runs:/d104fc5e1dd8470a8dde5b0c7a760814/model'
 # model = mlflow.pyfunc.load_model(model_uri)
