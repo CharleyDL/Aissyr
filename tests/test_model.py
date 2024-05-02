250afcd96b9da0e8c8a733910afdf07e6e94a444
Charley ∆. L. <CharleyDL@users.noreply.github.com>
2024-05-02 13:49:19 +0200

    test get envvar

--- tests/test_model.py
+++ tests/test_model.py
@@ -5,17 +5,17 @@
 # Date Created : Wednesday 1 May 2024
 # ==============================================================================
 
-# import os
+import os
 
 # from utils.functions import load_model
 
 
-# class TestLoadModel:
+class TestLoadModel:
 
-#     def test_get_envvar(self):
-#         assert os.environ.get("DAGSHUB_REPO_OWNER") is not None
-#         assert os.environ.get("DAGSHUB_REPO") is not None
-#         assert os.environ.get("MODEL_URI") is not None
+    def test_get_envvar(self):
+        assert os.environ.get("DAGSHUB_REPO_OWNER") is not None
+        assert os.environ.get("DAGSHUB_REPO") is not None
+        assert os.environ.get("MODEL_URI") is not None
 
 #     def test_load_model(self):
 #         model = load_model()
